2c9811cdffe76c89a2033c01427d3b2025062901
Grace Couch <gracetcouch@gmail.com>
2025-02-25 16:54:44 +0000

    initial commit

---
+++ AlarmApp/AlarmAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  AlarmAppApp.swift
+//  AlarmApp
+//
+//  Created by Grace couch on 25/02/2025.
+//
+
+import SwiftUI
+
+@main
+struct AlarmAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AlarmAppTests/AlarmAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  AlarmAppTests.swift
+//  AlarmAppTests
+//
+//  Created by Grace couch on 25/02/2025.
+//
+
+import Testing
+@testable import AlarmApp
+
+struct AlarmAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
