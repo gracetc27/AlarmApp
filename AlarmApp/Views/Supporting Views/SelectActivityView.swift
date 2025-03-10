22eb4888a1f79d6be6b33aeb58c0bc0f83f607f8
Grace Couch <gracetcouch@gmail.com>
2025-03-10 13:05:18 +0000

    ui touch ups

--- AlarmApp/Views/Supporting Views/SelectActivityView.swift
+++ AlarmApp/Views/Supporting Views/SelectActivityView.swift
@@ -20,7 +20,7 @@ struct SelectActivityView: View {
     var body: some View {
         HStack {
             ScrollView(.horizontal, showsIndicators: false) {
-                HStack {
+                HStack(spacing: 15) {
                     ForEach(activities, id: \.self) { activity in
                         let isSelected = activity == currentActivity
                         Image(systemName: activity)
@@ -35,12 +35,20 @@ struct SelectActivityView: View {
                     }
                 }
             }
+            Divider()
+                .frame(height: 20)
             Circle()
                 .fill(currentColor)
                 .frame(width: 20, height: 20)
                 .onTapGesture {
                     currentColorIndex = nextIndex
                 }
+        }
+        .padding(4)
+        .overlay {
+            RoundedRectangle(cornerRadius: 15)
+                .stroke(.gray, lineWidth: 2)
+
         }
     }
 }
