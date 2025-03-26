d7c54eff2ea0d456664db264555bd517b7ecbb60
Grace Couch <gracetcouch@gmail.com>
2025-03-26 14:01:23 +0000

    font constants

---
+++ AlarmApp/Constants/Font Constants.swift
@@ -0,0 +1,8 @@
+//
+//  Font Constants.swift
+//  AlarmApp
+//
+//  Created by Grace couch on 26/03/2025.
+//
+let fontVariable = "WorkSans-VariableFont_wght"
+let fontItalic = "WorkSans-Italic-VariableFont_wght"
